52cdf7ca556bed89acb74736b75acb7cc5c37412
tuguldur <aa402115@gmail.com>
2021-04-23 17:48:32 +0800

    removed seeding when deploy, header actions

--- front/src/components/header/index.js
+++ front/src/components/header/index.js
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
-import { Button, Container, Icon, LinearProgress } from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import {
+  Button,
+  Container,
+  Icon,
+  LinearProgress,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
 import Cookie from "js-cookie";
 import { Link } from "react-router-dom";
 import "./style.scss";
 import { User } from "global/user";
+import axios from "axios";
 const Header = () => {
   const { user } = useContext(User);
+  const [account, setAccount] = useState(false);
   return (
     <div id="header">
       {Cookie.get("session") && !user ? (
@@ -25,14 +34,34 @@ const Header = () => {
           <div className="header-col">
             <div className="header-item">
               {user ? (
-                <Button variant="outlined" color="primary">
-                  <img
-                    src={user.avatar}
-                    className="avatar"
-                    alt={`${user.name}'s avatar`}
-                  />
-                  {user.name}
-                </Button>
+                <div>
+                  <Button
+                    className="header-action"
+                    onClick={(e) => setAccount(e.currentTarget)}
+                  >
+                    <img src={user.avatar} className="avatar" alt={""} />
+                    {user.name}
+                  </Button>
+                  <Menu
+                    keepMounted
+                    open={Boolean(account)}
+                    anchorEl={account}
+                    onClose={() => setAccount(null)}
+                    onClick={() => setAccount(null)}
+                  >
+                    <MenuItem>Mэдээлэл</MenuItem>
+                    <MenuItem>Tохиргоо</MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        axios.get("/rest/auth/logout");
+                        Cookie.remove("session");
+                        document.location.reload();
+                      }}
+                    >
+                      Гарах
+                    </MenuItem>
+                  </Menu>
+                </div>
               ) : (
                 <Link to="/login">
                   <Button variant="contained" color="primary">
